f9ffd36b02cf8434d83fdc3d24e83174d5368f4c
reemake <safnikita@inbox.ru>
2022-11-11 21:58:29 +0300

    Initial commit

---
+++ src/main/java/ru/codemark/userssoapservice/UsersSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.codemark.userssoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/codemark/userssoapservice/UsersSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.codemark.userssoapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
